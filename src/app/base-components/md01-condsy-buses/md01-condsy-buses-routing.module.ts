dcf4b3220c3912c8c872e9559118719232e750a9
johan <johanmamani@upeu.edu.pe>
2019-11-05 15:39:06 -0500

    bus component

--- src/app/base-components/md01-condsy-buses/md01-condsy-buses-routing.module.ts
+++ src/app/base-components/md01-condsy-buses/md01-condsy-buses-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RequisitoComponent } from './requisito/requisito.component';
+import { BusesComponent } from './buses/buses.component';
+
 
 
 const routes: Routes = [
   {path: 'vinculacion', loadChildren: () => import('src/app/base-components/md01-condsy-buses/vinculacion/vinculacion.module').then(m => m.VinculacionModule)},
-  {path: 'requisito', component: RequisitoComponent}
+  {path: 'requisito', component: RequisitoComponent},
+  {path: 'buses' , component: BusesComponent}
 ];
 
 @NgModule({
--- src/app/base-components/md01-condsy-buses/md01-condsy-buses.module.ts
+++ src/app/base-components/md01-condsy-buses/md01-condsy-buses.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { MD01CondsyBusesRoutingModule } from './md01-condsy-buses-routing.module';
 import { ConductorComponent } from './conductor/conductor.component';
 import { RequisitoComponent } from './requisito/requisito.component';
-
+import { BusesComponent } from './buses/buses.component';
+ 
 @NgModule({
-  declarations: [ConductorComponent, RequisitoComponent],
+  declarations: [ConductorComponent, RequisitoComponent, BusesComponent],
   imports: [
     CommonModule,
     MD01CondsyBusesRoutingModule
