c46e8a8ad5ccdaef160c2196ad30bcfd0502c58d
gabrielcosi <gabrielcosi18@gmail.com>
2019-11-06 06:49:12 -0500

    Requisito

--- src/app/base-services/service.service.ts
+++ src/app/base-services/service.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { Persona } from '../base-models/Persona';
 import { TipoDocumento } from '../base-models/TipoDocumento';
 import { Requisito } from '../base-models/Requisito';
+import { TipoRequisito } from '../base-models/TipoRequisito';
 import { Usuario } from '../base-models/Usuario';
 import { Bus } from '../base-models/Bus';
 
@@ -19,6 +20,7 @@ export class ServiceService {
   tipoDocumento = 'http://localhost:8090/tipoDocumento/'
   buses = 'http://localhost:8090/bus/'
   requisitos = 'http://localhost:8090/requisito/'
+  tipoRequisito = 'http://localhost:8090/tipoRequisito/'
   seguridad = 'http://localhost:8090/seguridad/'
   
   getPersona(): Observable<Persona[]>{
@@ -41,6 +43,21 @@ export class ServiceService {
   }
   getRequisito(): Observable<Requisito[]>{
     return this.http.get<Requisito[]>(this.requisitos);
+  }
+  getRequisitoId(idrequisito: number): Observable<Requisito[]>{
+    return this.http.get<Requisito[]>(this.requisitos+idrequisito);
+  }
+  createRequisito(requisito: Requisito){
+    return this.http.post<Requisito>(this.requisitos+'add',requisito);
+  }
+  updateRequisito(requisito: Requisito) {
+    return this.http.put<Requisito>(this.requisitos +requisito.id_requisito, requisito);
+  }
+  deleteRequisito(requisito: Requisito) {
+    return this.http.delete<Requisito>(this.requisitos+requisito.id_requisito);
+  }
+  getTipoRequisito(): Observable<TipoRequisito[]>{
+    return this.http.get<TipoRequisito[]>(this.tipoRequisito);
   }
   getUsuario(): Observable<Usuario[]>{
     return this.http.get<Usuario[]>(this.seguridad);
