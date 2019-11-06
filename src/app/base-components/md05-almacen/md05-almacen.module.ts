f8f30fc14f93d4b3e16fe871fd7e57fc7149079c
Acuario11 <madaichuco@upeu.edu.pe>
2019-11-06 03:07:23 -0500

    subiendo componente

--- src/app/base-components/md05-almacen/md05-almacen.module.ts
+++ src/app/base-components/md05-almacen/md05-almacen.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { Md05AlmacenRoutingModule } from './md05-almacen-routing.module';
 import { ProductoComponent } from './producto/producto.component';
 
 @NgModule({
   declarations: [ProductoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    Md05AlmacenRoutingModule
   ]
 })
 export class Md05AlmacenModule { }
