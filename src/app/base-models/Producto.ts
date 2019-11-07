326b44e67026e9d27f2888d9ba7ca4230009cfcc
Acuario11 <madaichuco@upeu.edu.pe>
2019-11-07 10:34:48 -0500

    probando

---
+++ src/app/base-models/Producto.ts
@@ -0,0 +1,9 @@
+export class Producto{
+    id_producto:   number;
+    nombre_producto: string;
+    descripcion: string;
+    id_categoria: number;
+    id_unidad_medida: number;
+    codigo: string;
+    id_marca: number;
+}
