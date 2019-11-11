48b85b43538969904d716c5a5d32cd547cacccfe
Adrian <adrianorihuela@upeu.edu.pe>
2019-11-10 21:42:06 -0500

    actualizando

--- src/app/base-components/md05-almacen/md05-almacen.module.ts
+++ src/app/base-components/md05-almacen/md05-almacen.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-<<<<<<< HEAD
 import { Md05AlmacenRoutingModule } from './md05-almacen-routing.module';
 import { ProductoComponent } from './producto/producto.component';import { FormsModule } from '@angular/forms';
 
@@ -13,22 +12,6 @@ import { ProductoComponent } from './producto/producto.component';import { Forms
     CommonModule,
     Md05AlmacenRoutingModule,
     FormsModule
-=======
-import { ProductoComponent } from './producto/producto.component';
-import { Md05AlmacenRoutingModule } from './md05-almacen-routing.module';
-import { CategoriaComponent } from './categoria/categoria.component';
-import { FormsModule } from '@angular/forms';
-import { NgbDate, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-
-@NgModule({
-  declarations: [ProductoComponent, CategoriaComponent],
-  imports: [
-    CommonModule,
-    Md05AlmacenRoutingModule,
-    FormsModule,
-    NgbModule
->>>>>>> df8c6a0873e83fac9f064b2f4d7066b7d68d3e5f
   ]
 })
 export class Md05AlmacenModule { }
