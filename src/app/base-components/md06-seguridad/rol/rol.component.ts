a1d196555cc1c1373c6ef4dafe2bd74f03186b5d
gabrielcosi <gabrielcosi18@gmail.com>
2019-11-12 13:56:56 -0500

    k px

--- src/app/base-components/md06-seguridad/rol/rol.component.ts
+++ src/app/base-components/md06-seguridad/rol/rol.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServiceService } from 'src/app/base-services/service.service';
+import { Rol } from 'src/app/base-models/Rol';
 
 @Component({
   selector: 'app-rol',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rol.component.scss']
 })
 export class RolComponent implements OnInit {
-
-  constructor() { }
+  rol: Rol = new Rol();
+  listRoles: Rol[] = [];
+  constructor(private service: ServiceService, private router: Router) { }
 
   ngOnInit() {
+    this.service.getRol().subscribe(data => {
+      this.listRoles=data['rols']
+    })
   }
-
+  Guardar() {
+    this.service.createRol(this.rol).subscribe(data => {
+      alert('Registro guardado correctamente...!');
+      this.ngOnInit();
+    })
+  }
 }
