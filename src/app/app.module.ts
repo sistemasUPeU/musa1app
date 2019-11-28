9e18d3929970695904384286da3ad354045870ab
unknown <jorgemaquerac@upeu.edu.pe>
2019-11-28 17:06:14 -0500

    Revisar app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,34 @@
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LanguageTranslationModule } from './shared/modules/language-translation/language-translation.module';
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LanguageTranslationModule } from "./shared/modules/language-translation/language-translation.module";
 
-
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AuthGuard } from './shared';
-import { ServiceService } from 'src/app/base-services/service.service';
-import {EncrDecrService} from 'src/app/base-services/encr-decr.service';
-import { GrupoService } from 'src/app/base-services/grupo/grupo.service';
-import { FilterBusPipe } from './Search/filter-bus.pipe';
-<<<<<<< HEAD
-import { FilterTipoMantenimientoPipe } from './Search/filter-tipo-mantenimiento.pipe';
-import { FilterMantenimientoPipe } from './Search/filter-mantenimiento.pipe';
-import { NgxPaginationModule } from 'ngx-pagination';
-
-
-
-=======
-import { FilterProductoPipe } from './search/filter-producto.pipe';
-import { FilterRevisionPipe } from './search/filter-revision.pipe';
->>>>>>> prueba
-
-
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AuthGuard } from "./shared";
+import { ServiceService } from "src/app/base-services/service.service";
+import { EncrDecrService } from "src/app/base-services/encr-decr.service";
+import { GrupoService } from "src/app/base-services/grupo/grupo.service";
+import { FilterBusPipe } from "./Search/filter-bus.pipe";
+import { FilterTipoMantenimientoPipe } from "./Search/filter-tipo-mantenimiento.pipe";
+import { FilterMantenimientoPipe } from "./Search/filter-mantenimiento.pipe";
+// import { NgxPaginationModule } from 'ngx-pagination';
+import { FilterProductoPipe } from "./search/filter-producto.pipe";
+import { FilterRevisionPipe } from "./search/filter-revision.pipe";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        BrowserModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        LanguageTranslationModule,
-        AppRoutingModule
-    ],
-    declarations: [AppComponent,],
-    providers: [AuthGuard, ServiceService, GrupoService, EncrDecrService],
-    bootstrap: [AppComponent]
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    LanguageTranslationModule,
+    AppRoutingModule
+  ],
+  declarations: [AppComponent],
+  providers: [AuthGuard, ServiceService, GrupoService, EncrDecrService],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
