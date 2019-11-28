d8f30ecd457cc8b8cb60a4a40355d693044952b8
JoseCV09 <josechacpa@upeu.edu.pe>
2019-11-28 15:45:03 -0500

    arreglado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,17 +14,8 @@ import { ServiceService } from 'src/app/base-services/service.service';
 import {EncrDecrService} from 'src/app/base-services/encr-decr.service';
 import { GrupoService } from 'src/app/base-services/grupo/grupo.service';
 import { FilterBusPipe } from './Search/filter-bus.pipe';
-<<<<<<< HEAD
-import { FilterTipoMantenimientoPipe } from './Search/filter-tipo-mantenimiento.pipe';
-import { FilterMantenimientoPipe } from './Search/filter-mantenimiento.pipe';
-import { NgxPaginationModule } from 'ngx-pagination';
-
-
-
-=======
 import { FilterProductoPipe } from './search/filter-producto.pipe';
 import { FilterRevisionPipe } from './search/filter-revision.pipe';
->>>>>>> prueba
 
 
 
