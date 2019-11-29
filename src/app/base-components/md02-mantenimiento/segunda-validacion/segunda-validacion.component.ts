bcdcd552bff56691dffd79bc1dd0859e38341665
Alumno Laboratorio 6 DIGETI <alumno.l6@lim.upeu.edu.pe>
2019-11-28 20:05:44 -0500

    asustado potter? calculo git add .

--- src/app/base-components/md02-mantenimiento/segunda-validacion/segunda-validacion.component.ts
+++ src/app/base-components/md02-mantenimiento/segunda-validacion/segunda-validacion.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ServiceService } from 'src/app/base-services/service.service';
 import { Mantenimiento } from 'src/app/base-models/Mantenimiento';
 import { DetalleMantenimiento } from 'src/app/base-models/DetalleMantenimiento';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-segunda-validacion',
@@ -14,7 +15,7 @@ export class SegundaValidacionComponent implements OnInit {
   cargarmantenimiento: Mantenimiento[]=[];
   cargarmantenimientodata: Mantenimiento[]=[];
 
-  constructor(private service: ServiceService) { }
+  constructor(private service: ServiceService, private router: Router) { }
 
   ngOnInit() {
     /*Lista todo los datos provenientes de Validacion de jefe de mantenimiento*/
@@ -29,6 +30,7 @@ export class SegundaValidacionComponent implements OnInit {
     this.service.updateValidar(mantenimiento).subscribe(data => {  
       console.log(data);
       alert("¡ Ahora el mecanico esta autorizado para realizar sus pedidos !")
+      document.getElementById("fila").style.background="rgb(171, 223, 171)";
       this.ngOnInit();
     })
   }
@@ -37,6 +39,7 @@ export class SegundaValidacionComponent implements OnInit {
     console.log(mantenimiento.id_mantenimiento);
     this.service.updateValidar(mantenimiento).subscribe(data => {  
       this.ngOnInit();
+      alert("¡ El registro a sido denegado!")
     })
   }
 
